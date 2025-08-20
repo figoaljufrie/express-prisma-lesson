1fdf755a06cf174c7d68e6ee67e663236c306401
figoaljufrie <figoaldjoeffri@gmail.com>
2025-08-20 12:33:43 +0700

    feat: forgot-password

---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { AuthController } from "./auth.controller";
+export class AuthRouter {
+  private router: Router;
+  private authController: AuthController;
+  
+  constructor() {
+    this.router = Router();
+    this.authController = new AuthController();
+    this.initializedRoutes();
+  }
+
+  private initializedRoutes = () => {
+    this.router.post(
+      "/forgot-password",
+      this.authController.forgotPassword
+    );
+    
+  };
+
+  getRouter = () => {
+    return this.router;
+  };
+}
